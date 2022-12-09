78442dfa77ae6c7066d5a5dfacb9377c190f9598
Eduardo Fakiani <efakiani@gmail.com>
2022-12-09 04:57:15 -0500

    Re-factor V1

--- app.py
+++ app.py
@@ -1,30 +1,21 @@
+import names
+import starkbank
 from flask import Flask
 from flask.globals import request
+from random import randint
+from cpf_generator import CPF
+from datetime import datetime, timedelta
 from flask_apscheduler import APScheduler
 
-import starkbank
-
-from datetime import datetime, timedelta
-from cpf_generator import CPF
-from random import randint
-
-import names
-
-from dotenv import load_dotenv
-import os
-
-load_dotenv()
-
-CHAVE_PRIVADA = os.getenv("CHAVE_PRIVADA")
-AMBIENTE = os.getenv("AMBIENTE")
-ID_PROJETO = os.getenv("ID_PROJETO")
-MAIN_ACCOUNT = os.getenv("MAIN_ACCOUNT")
-
+ambiente = ""
+idProjeto = ""
+chavePrivada = ""
+contaPrincipal = ""
 
 # Criacao da variavel project para uso da API
-projeto = starkbank.Project(environment=AMBIENTE,
-                            id=ID_PROJETO,
-                            private_key=CHAVE_PRIVADA)
+projeto = starkbank.Project(environment=ambiente,
+                            id=idProjeto,
+                            private_key=chavePrivada)
 
 starkbank.user = projeto
 app = Flask(__name__)
@@ -34,16 +25,14 @@ scheduler = APScheduler()
 scheduler.init_app(app)
 scheduler.start()
 
-
 # Funcao para transferir valor determinado para conta principal stark
 def transferir(valor, nome):
     transferencia = starkbank.Transaction(
         amount=valor,
-        receiver_id=MAIN_ACCOUNT,
+        receiver_id=contaPrincipal,
         description=f"{nome} | Carteira Intermediaria -> Carteira Principal",
         external_id=str(randint(1000000, 100000000)))
     starkbank.transaction.create([transferencia])
-
 
 # Webhook para verificar e atualizar a cada invoice
 @app.route('/hook', methods=['POST'])
@@ -65,7 +54,6 @@ def hook():
             transferir(valor - 50, nome)
         return 'OK', 200
 
-
 # Executar funcao a cada 3 horas e enviar de 8 a 12 invoices
 @scheduler.task("interval", id='invoices', hours=3)
 def gerar_invoices():
@@ -83,10 +71,8 @@ def gerar_invoices():
                               interest=1.5))
 
     starkbank.invoice.create(invoice_list)
-
     return None
-
 
 if __name__ == '__main__':
     gerar_invoices()
-    #app.run(host='0.0.0.0', port=80)
+    app.run(host='0.0.0.0', port=80)
